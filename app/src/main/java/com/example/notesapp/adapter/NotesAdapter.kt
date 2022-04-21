78903077520573f45e3b797970d93f2cec2fd2d1
vuconfused <vut30330@gmail.com>
2022-04-21 17:57:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/adapter/NotesAdapter.kt
@@ -0,0 +1,134 @@
+package com.example.notesapp.adapter
+
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notesapp.entities.Notes
+import com.example.notesapp.R
+import kotlinx.android.synthetic.main.fragment_create_note.view.*
+import kotlinx.android.synthetic.main.item_rv_notes.view.*
+import kotlinx.android.synthetic.main.item_rv_notes.view.imgNote
+import kotlinx.android.synthetic.main.item_rv_notes.view.tvDateTime
+
+
+class NotesAdapter() :
+    RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
+    var listener:OnItemClickListener? = null
+    var arrList = ArrayList<Notes>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
+        return NotesViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_rv_notes,parent,false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return arrList.size
+    }
+
+    fun setData(arrNotesList: List<Notes>){
+        arrList = arrNotesList as ArrayList<Notes>
+    }
+
+    fun setOnClickListener(listener1: OnItemClickListener){
+        listener = listener1
+    }
+
+    override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
+
+        holder.itemView.tvTitle.text = arrList[position].title
+        holder.itemView.tvDesc.text = arrList[position].noteText
+        holder.itemView.tvDateTime.text = arrList[position].dateTime
+
+        if (arrList[position].color != null){
+            holder.itemView.cardView.setCardBackgroundColor(Color.parseColor(arrList[position].color))
+        }else{
+            holder.itemView.cardView.setCardBackgroundColor(Color.parseColor(R.color.lightBlack.toString()))
+        }
+
+        if (arrList[position].imgPath != null){
+            holder.itemView.imgNote.setImageBitmap(BitmapFactory.decodeFile(arrList[position].imgPath))
+            holder.itemView.imgNote.visibility = View.VISIBLE
+        }else{
+            holder.itemView.imgNote.visibility = View.GONE
+        }
+
+        if (arrList[position].webLink != ""){
+            holder.itemView.tvWebLink.text = arrList[position].webLink
+            holder.itemView.tvWebLink.visibility = View.VISIBLE
+        }else{
+            holder.itemView.tvWebLink.visibility = View.GONE
+        }
+
+        holder.itemView.cardView.setOnClickListener {
+            listener!!.onClicked(arrList[position].id!!)
+        }
+
+    }
+
+    class NotesViewHolder(view:View) : RecyclerView.ViewHolder(view){
+
+    }
+
+
+    interface OnItemClickListener{
+        fun onClicked(noteId:Int)
+    }
+
+}
+
+//class NotesAdapter(val arrList: List<Notes>) :
+//
+//    RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
+//
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
+//
+//        return NotesViewHolder(
+//            LayoutInflater.from(parent.context).inflate(R.layout.item_rv_notes,parent,false)
+//        )
+//    }
+//
+//    override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
+//        holder.itemView.tvTitle.text = arrList[position].title
+//        holder.itemView.tvDesc.text = arrList[position].noteText
+//        holder.itemView.tvDateTime.text = arrList[position].dateTime
+//
+////        if (arrList[position].color != null){
+////            holder.itemView.cardView.setCardBackgroundColor(Color.parseColor(arrList[position].color))
+////        }else{
+////            holder.itemView.cardView.setCardBackgroundColor(Color.parseColor(R.color.lightBlack.toString()))
+////        }
+////
+////        if (arrList[position].imgPath != null){
+////            holder.itemView.imgNote.setImageBitmap(BitmapFactory.decodeFile(arrList[position].imgPath))
+////            holder.itemView.imgNote.visibility = View.VISIBLE
+////        }else{
+////            holder.itemView.imgNote.visibility = View.GONE
+////        }
+////
+////        if (arrList[position].webLink != ""){
+////            holder.itemView.tvWebLink.text = arrList[position].webLink
+////            holder.itemView.tvWebLink.visibility = View.VISIBLE
+////        }else{
+////            holder.itemView.tvWebLink.visibility = View.GONE
+////        }
+////
+////        holder.itemView.cardView.setOnClickListener {
+////            listener!!.onClicked(arrList[position].id!!)
+////        }
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return arrList.size
+//    }
+//
+//    class NotesViewHolder(view:View) : RecyclerView.ViewHolder(view){
+//
+//    }
+//    interface OnItemClickListener{
+//        fun onClicked(noteId:Int)
+//    }
+//}
